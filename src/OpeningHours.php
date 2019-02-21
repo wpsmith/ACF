e7a47335015f0e65fa0e33bec25377573d4307ae
wpsmith <t@wpsmith.net>
2019-02-20 19:25:58 -0500

    Update copyright

--- src/OpeningHours.php
+++ src/OpeningHours.php
@@ -10,7 +10,7 @@
  *
  * @package    WPS\WP\Plugins\ACF
  * @author     Travis Smith <t@wpsmith.net>
- * @copyright  2015-2018 Travis Smith
+ * @copyright  2015-2019 Travis Smith
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License v2
  * @link       https://github.com/wpsmith/WPS
  * @version    1.0.0
